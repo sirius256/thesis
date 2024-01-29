10d8af615357f2f72057b9457bd4567e2f1a09d9
sirius256 <gifon.24@gmail.com>
2024-01-29 05:28:52 +0200

    Update profile view page.

--- resources/views/main/pages/administration/viewProfile.blade.php
+++ resources/views/main/pages/administration/viewProfile.blade.php
@@ -2,10 +2,16 @@
 
 @section('content')
     <div class="admin-page-content">
-        <div>Нікнейм: {{ $user->name }}</div>
+        <div class="mb-2">
+            <div>Нікнейм: {{ $user->name }}</div>
+            <div>Емейл: {{ $user->email }}</div>
+            <div>Дата реєстрації: {{ $user->created_at }}</div>
+        </div>
 
-        <a href="{{ route('profile.edit') }} "class="btn btn-primary">
-            {{ __('administration.menu-item.profile-edit') }}
-        </a>
+        <div>
+            <a href="{{ route('profile.edit') }} "class="btn btn-primary">
+                {{ __('administration.menu-item.profile-edit') }}
+            </a>
+        </div>
     </div>
  @endsection
