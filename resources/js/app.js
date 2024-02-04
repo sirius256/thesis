3d4c05f5e51020c89da8a9bd0a4b91ac955fe329
sirius256 <gifon.24@gmail.com>
2024-02-04 02:17:35 +0200

    Include fortawesome from node modules. Fix css.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import './bootstrap';
 import '../css/bootstrap.min.css';
 import '../css/device.css';
+import '../../node_modules/@fortawesome/fontawesome-free/css/all.css';
 
 import Alpine from 'alpinejs';
 
--- resources/views/main/layouts/base.blade.php
+++ resources/views/main/layouts/base.blade.php
@@ -7,9 +7,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link href="{{ asset('/css/fontawesome-free-6.5.1-web/css/fontawesome.css') }}" rel="stylesheet">
-    <link href="{{ asset('/css/fontawesome-free-6.5.1-web/css/brands.css') }}" rel="stylesheet">
-    <link href="{{ asset('/css/fontawesome-free-6.5.1-web/css/solid.css') }}" rel="stylesheet">
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
